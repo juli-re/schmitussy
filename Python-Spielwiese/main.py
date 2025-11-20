11149e97620057d7aaefb58ea2b8a47de30d14c6
SchmidtYannic <test@freak.de>
2025-11-20 14:25:05 +0100

    Random Bullshit

--- Python-Spielwiese/main.py
+++ Python-Spielwiese/main.py
@@ -1,3 +1,5 @@
+#19.11.2025
+
 Test = [0, 1, 4, 3]
 print ("servus")
 print (Test)
@@ -5,4 +7,12 @@ print (type (Test))
 for element in Test:
     print(Test[2])
 
-#test2
+#20.11.2025
+
+person = {"name": "Schmidt", "vorname": "Yannic", "alter": 25}
+print (person)
+print ("Nachname: " + person["name"])
+person2 = {"name": "Reiser", "vorname": "Julian", "alter": 50}
+Index = [person, person2]
+for x in Index:
+    print (x["vorname"])
